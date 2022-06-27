fba7f4185cd9c19baa949a4485938aa3e8055274
Abdumalik07 <104436538+Abdumalik07@users.noreply.github.com>
2022-06-27 06:04:15 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.function.UnaryOperator;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/* sqrt деген метод тузунуз.
+UnaryOperator деген интерфейстин реализациясын кайтаруусу керек
+Бир санды кабыл алып анын тамырдан чыккан жообун кайтарат(корень). */
+
+        System.out.println(sqrt().apply(16d));
+
+    }
+    public static UnaryOperator<Double> sqrt(){
+        return t -> Math.sqrt(t);
+    }
+
+}
